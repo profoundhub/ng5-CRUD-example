a06e592e99afe54524fc46367737bfaedd73c7b2
Daniel <github@profoundideation.com>
2018-04-29 11:51:10 -0400

    ++ book.js

--- mean-crud/models/Book.js
+++ mean-crud/models/Book.js
@@ -1,1 +1,13 @@
 let mongoose = require('mongoose');
+
+let BookSchema = new mongoose.Schema({
+    isbn: String,
+    title: String,
+    author: String,
+    description: String,
+    published_year: String,
+    publisher: String,
+    updated_date: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Book', BookSchema);
