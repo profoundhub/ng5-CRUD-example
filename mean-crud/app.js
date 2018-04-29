1dd256341d061bd9b54bff7752cdfe3e999eb46b
Daniel <github@profoundideation.com>
2018-04-29 11:24:38 -0400

    update time

--- mean-crud/app.js
+++ mean-crud/app.js
@@ -16,7 +16,7 @@ app.use('/book', book);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    let err = new Error('Not Found');
+    var err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
